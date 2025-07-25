4a5dea0d94a5589d07de26703a5f019860cf86bb
Ronny Krite <rkrite@gmail.com>
2025-07-25 14:41:47 +1000

    css layouts

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -3,15 +3,26 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="icon" type="image/png" href="/favicon.ico">
         <title>VBox Data Converter</title>
+        {{-- @vite('resources/css/app.css') --}}
         {{-- <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script> --}}
         <script src="/css/rkt-tw-css.js"></script>
         <style type="text/tailwindcss">
-          @theme {
-            --color-greyish: #888;
-            --color-lightgrey: #ccc;
-            --color-blueish: #257;
-            --color-redish: #725;
-          }
+            @theme {
+                --color-greyish: #888;
+                --color-lightgrey: #ccc;
+                --color-blueish: #257;
+                --color-redish: #725;
+            }
         </style>
     </head>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-FMN4D79LW1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-FMN4D79LW1');
+</script>
